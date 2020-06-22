aab23d6865ac694dde2f4616f8a39b7277529e6f
UJJWAL CHAUDHARY <ujjwalanju2001@gmail.com>
2020-06-22 22:34:27 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void CreatBoard(void);    // creat ttt board
+void WinCheck(void);    // check for win
+
+int main()
+{   // main code
+    
+    return 0;
+}
+
+void CreatBoard(void)
+{
+    
+    // write defination here
+}
+
+
+void WinCheck(void)
+{
+    
+    
+    // write defination here
+}
